ebd51a6e82cbe63315a5dd227801a2dd9f21bbdc
princeprakhar <prakhardeep173@gmail.com>
2024-10-20 20:45:46 +0530

    fixes dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "@pages/Home";
-import ProtectedRoute from "@pages/ProtectedRoute";
-import SendMoney from "@pages/SendMoney";
-import SignIn from "@pages/SignIn";
-import SignUp from "@pages/SignUp";
-import Dashboard from "@pages/Dashboard";
+import Home from "@/pages/Home";
+import ProtectedRoute from "@/pages/ProtectedRoute";
+import SendMoney from "@/pages/SendMoney";
+import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+import DashBoard from "@/pages/DashBoard";
 function App() {
   return (
     <div>
@@ -13,7 +13,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard" element={<DashBoard />} />
           </Route>
           <Route path="/send" element={<SendMoney />} />
           <Route path={"/" || "/home"} element={<Home />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import HeroSection from '@components/HeroSection';
-import Navbar from '@components/Navbar';
-import Footer from '@components/Footer';
+import HeroSection from '@/components/HeroSection';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const Home = ({ children }) => {
 
