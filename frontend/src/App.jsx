91b9a82f9a58f0c00dd7c8102adf0bfe4b7c63e9
princeprakhar <prakhardeep173@gmail.com>
2024-10-20 20:40:33 +0530

    added send functionality to dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import ProtectedRoute from "./pages/ProtectedRoute";
-import SendMoney from "./pages/SendMoney";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Dashboard from "./pages/Dashboard";
+import Home from "@/pages/Home";
+import ProtectedRoute from "@/pages/ProtectedRoute";
+import SendMoney from "@/pages/SendMoney";
+import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+import Dashboard from "@/pages/Dashboard";
 function App() {
   return (
     <div>
